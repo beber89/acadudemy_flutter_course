2534ced68fd7d065b2e44dd0362363f1a0fb0baf
Moudbuom <mahmoudbaibars@gmail.com>
2019-05-03 16:17:22 +0200

    Initial commit - starting from video #073

---
+++ lib/pages/product.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class ProductPage extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+
+  ProductPage(this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope( 
+      // Widget is created to provide more control on back navigation
+      onWillPop: () {
+        print('Back button pressed!');
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(imageUrl),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: Text(title),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                color: Theme.of(context).accentColor,
+                child: Text('DELETE'),
+                onPressed: () => Navigator.pop(context, true),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
