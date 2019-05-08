cdd4b88727f89051c24da8a9ba85a81219bf0ac4
Moudbuom <mahmoudbaibars@gmail.com>
2019-05-08 02:09:18 +0200

    finished up section 6: navigation

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -4,6 +4,11 @@ import '../product_manager.dart';
 import './products_admin.dart';
 
 class ProductsPage extends StatelessWidget {
+  final List<Map<String, String>> products;
+  final Function addProduct;
+  final Function deleteProduct;
+
+  ProductsPage(this.products, this.addProduct, this.deleteProduct);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,11 +22,8 @@ class ProductsPage extends StatelessWidget {
             ListTile(
               title: Text('Manage Products'),
               onTap: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) =>
-                            ProductsAdminPage()));
+                Navigator.pushReplacementNamed(
+                    context, '/admin');
               },
             )
           ],
